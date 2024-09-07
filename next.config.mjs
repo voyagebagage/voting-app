d20868a6328f4dfa78858f734e88b69eee745f58
Olivier Frugier <voyagebagageao@gmail.com>
2024-09-07 20:17:30 +0700

    change tlegram types

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@twa-dev/sdk"],
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+    });
+    return config;
+  },
   env: {
     NEXT_PUBLIC_TELEGRAM_BOT_NAME: process.env.NEXT_PUBLIC_TELEGRAM_BOT_NAME,
     TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
